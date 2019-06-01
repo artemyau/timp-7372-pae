eaf8c03ed778b4ac611d427755d18d992f0d49fd
Artem Polovnikov <artempol99@mail.ru>
2019-06-01 11:35:15 +0700

    Добавлены файлы с решением первой практики 	new file:   01_hello_world.c 	new file:   02_sum.c 	new file:   03_pow.c 	modified:   README.md

---
+++ 02_sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{	int a,b,result;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	result=a+b;
+	printf("%d\n",result);
+	return 0;}
---
+++ 03_pow.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int a, b, result;
+scanf("%d", &a);
+scanf("%d", &b);
+result = pow(a, b);
+printf("%d\n", result);
+return 0;}
